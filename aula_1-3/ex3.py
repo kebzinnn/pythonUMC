5e1725812b43aeb19b60671698627398f3e81e09
kebzinnn <kleberlorensetijr@gmail.com>
2026-05-23 19:40:58 -0300

    #

--- aula_1-3/ex3.py
+++ aula_1-3/ex3.py
@@ -1,5 +1,5 @@
 #calculadora de celsius para kelvin
-celsius = int(input("Digite quantos graus celsius: "))
+celsius = int(input("Digite quantosfdsa graus celsius: "))
 
 kelvin = celsius + 273
 print(f"{celsius} graus Celsius equivale a {kelvin} graus Kelvin")
